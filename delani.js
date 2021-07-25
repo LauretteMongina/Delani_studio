3c014e318b5aacae27798eb06cb313c47980904c
laurettemongina <monginalaurette2gmail.com>
2021-07-25 13:07:00 +0300

    Adds a toogle effect of hide and show for the what we do icons, showing description.

---
+++ delani.js
@@ -0,0 +1,35 @@
+// what we do
+$(document).ready(function(){
+
+  $("#product_image").click(function(){
+    $('#product').show();
+    $('#product_image').hide();
+  });
+  $("#product").click(function(){
+    $('#product_image').show();
+    $('#product').hide();
+  });
+
+  $("#design_image").click(function(){
+    $('#design').show();
+    $('#design_image').hide();
+  });
+  $("#design").click(function(){
+    $('#design_image').show();
+    $('#design').hide();
+  });
+
+  $("#development_image").click(function(){
+    $('#development').show();
+    $('#development_image').hide();
+  });
+  $("#development").click(function(){
+    $('#development_image').show();
+    $('#development').hide();
+  });
+
+});
+// portfolio
+$(document).ready(function(){
+  
+})
